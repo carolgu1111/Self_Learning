b560cc843e26d0ff964316a23ddf0bf65f6b7446
carolgu1111 <85176644+carolgu1111@users.noreply.github.com>
2022-06-11 17:47:40 +0900

    Delete Ting directory

--- Ting/Ting/src/main/java/com/ruoyi/project/ums/employed/constant/UMSEmployedConst.java
+++
@@ -1,8 +0,0 @@
-package com.ruoyi.project.ums.employed.constant;
-
-public interface UMSEmployedConst {
-    // 公告类型名称唯一
-    String EMPLOYED_NAME_UNIQUE = "0";
-    // 公告类型名称不唯一
-    String EMPLOYED_NAME_NOT_UNIQUE = "1";
-}
